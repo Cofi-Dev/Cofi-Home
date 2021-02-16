819105a71ffcfa7ae439dfa6cbe8393634582ee4
victorst79 <victorst79@gmail.com>
2021-02-16 09:05:33 +0100

    added new views

---
+++ components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="nam-navbar">
+    <div class="nam-navbar__container">
+      <NuxtLink to="/">
+        Home
+      </NuxtLink>
+      <NuxtLink to="/about">
+        About
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NamNavBar'
+})
+</script>
+
+<style>
+/*  */
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,75 +1,28 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        NaM-Home
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div class="nam-home">
+    <div class="nam-home__nav">
+      <NamNavbar />
+    </div>
+    <div class="nam-home__container">
+      Contenido
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import NamNavbar from '@/components/NavBar.vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  components: {
+    NamNavbar
+  }
+})
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.nam-home {
+  display: grid;
+  grid-template-columns: 1fr;
 }
 </style>
